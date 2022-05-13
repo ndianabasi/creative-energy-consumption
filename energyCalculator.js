4a93ecb34184260832e33ca1cde5be3c940fbe07
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2022-05-13 11:14:36 +0100

    feat: log times

--- energyCalculator.js
+++ energyCalculator.js
@@ -1,47 +1,51 @@
 const renderText = text => {
-  const eventLog = document.querySelector('#event-log')
+  const eventLog = document.querySelector("#event-log");
 
-  const span = document.createElement('span')
+  const span = document.createElement("span");
   span.innerHTML = text;
 
-  eventLog.appendChild(span)
-}
+  eventLog.appendChild(span);
+};
 
 let documentVisibility = document.visibilityState;
 
-const startTime = performance.now()
+const startTime = performance.now();
 
-document.addEventListener('readystatechange', (event) => {
-  if (document.readyState === 'complete') {
-    if (documentVisibility === 'hidden') {
-      renderText('Ad was not visible during render')
-      return
+document.addEventListener("readystatechange", event => {
+  if (document.readyState === "complete") {
+    if (documentVisibility === "hidden") {
+      renderText("Ad was not visible during render");
+      return;
     }
 
-    const renderTime = event.timeStamp - startTime
+    const renderTime = event.timeStamp - startTime;
 
-    const watts = 100
-    const renderTimeInHours = (renderTime / (1000 * 60 * 60))
-    const wh = watts * renderTimeInHours
+    console.log({ renderTime, startTime });
 
-    renderText(`Ad rendered in ${renderTime.toFixed(2)}ms`)
-    renderText(`Energy consumption approximately ${wh.toString().substring(0,7)}Wh`)
+    const watts = 100;
+    const renderTimeInHours = renderTime / (1000 * 60 * 60);
+    const wh = watts * renderTimeInHours;
+
+    renderText(`Ad rendered in ${renderTime.toFixed(2)}ms`);
+    renderText(
+      `Energy consumption approximately ${wh.toString().substring(0, 7)}Wh`
+    );
   }
-})
+});
 
-document.addEventListener('visibilitychange', () => {
+document.addEventListener("visibilitychange", () => {
   documentVisibility = document.visibilityState;
-})
+});
 
-const observer = new PerformanceObserver((list) => {
+const observer = new PerformanceObserver(list => {
   console.log(list.getEntries());
   for (const entry of list.getEntries()) {
     if (entry.initiatorType === "fetch") {
-      console.log('Fetch request detected to', entry.name);
+      console.log("Fetch request detected to", entry.name);
     }
   }
 });
 
 observer.observe({
-  entryTypes: ['resource']
+  entryTypes: ["resource"]
 });
