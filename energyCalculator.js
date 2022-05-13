404ee95add2d3099a554eca5e5a6fe7095491399
Juuso Soikkeli <juuso.soikkeli@cavai.com>
2022-05-13 13:58:43 +0300

    mobile device detection

--- energyCalculator.js
+++ energyCalculator.js
@@ -7,17 +7,25 @@ const renderText = text => {
   eventLog.appendChild(span)
 }
 
-const observer = new PerformanceObserver((list) => {
-  const entries = list.getEntries()
-  const renderDuration = entries.find(({ entryType }) => entryType === 'navigation').duration
+const calculateEnergyConsumption = navigationEntry => {
+  const renderDuration = navigationEntry.duration
 
-  const watts = 100
+  const watts = !isMobile ? 100 : 5
   const renderTimeInHours = (renderDuration / (1000 * 60 * 60))
   const wh = watts * renderTimeInHours
 
   renderText(`Ad rendered in ${renderDuration.toFixed(2)}ms`)
   renderText(`Energy consumption approximately ${wh.toString().substring(0, 7)}Wh`)
+}
 
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
+const observer = new PerformanceObserver((list) => {
+  const entries = list.getEntries()
+  const navigationEntry = entries.find(({ entryType }) => entryType === 'navigation')
+  if (navigationEntry) {
+    calculateEnergyConsumption(navigationEntry)
+  }
 });
 
 observer.observe({
